e7e4115bb6b9685ed93d9ee0b1ca23bad0e8b605
LeandroYara <l.yara@uniandes.edu.co>
2021-09-06 08:48:31 -0500

    Update proyecto0.py

--- proyecto0.py
+++ proyecto0.py
@@ -11,6 +11,7 @@ commands = {
     'POP' : True,
     'CHECK' : True,
     'BLOCKEDP' : True,
+    '!BLOCKEDP' : True,
     'NOP' : True,
     'REPEAT' : True,
     'IF' : True,
@@ -18,6 +19,17 @@ commands = {
     'TO' : True,
     'OUTPUT' : True,
     'END' : True
+}
+
+numericParametersCommands = {
+    'MOVE' : True,
+    'RIGHT' : True,
+    'LEFT' : True,
+    'ROTATE' : True,
+    'DROP' : True,
+    'FREE' : True,
+    'PICK' : True,
+    'POP' : True,
 }
 
 #Lista de comandos para la instrucción Look
@@ -34,10 +46,22 @@ checkCommands = {
     'B' : True
 }
 
-#Variables definidas durante la ejecución del programa
-definedVariables = {
+#Funciones de verificacion
 
-}
+def alphabeticVariable(variable):
+    if variable.isalpha():
+        return True
+    else:
+        return False
+
+def integerValue(value):
+    if value.isnumeric():
+        return True
+    else:
+        return False
+
+#Variables definidas durante la ejecución del programa
+definedVariables= {}
 
 #Caracteres especiales
 specialChars = {
@@ -63,14 +87,43 @@ def verifyInstructions():
         for line in file:
             instructionsList += line #Se añade cada línea leída al String que contiene todo
     print("INICIO DE LA VERIFICACIÓN")
+    actualPosition = 0
+    value = 0
     lookInstruction = False # Centinela que espera la instrucción del LOOK
     validInstruction = True # Centinela que comprueba si el conjunto de instrucciones es correcto
     word = "" # Formación de comandos
     for char in instructionsList: #Recorremos cada una de las instrucciones
+        if word in numericParametersCommands.keys():
+            if instructionsList[actualPosition + 1].isdigit() == False:
+                validInstruction = False
         if word == "LOOK": #Verifica si el comando es LOOK
             lookInstruction = True
         if word in lookCommands.keys():
             lookInstruction = False
+        if word == "DEFINE": #Revisa la instruccion de definición
+            variable = instructionsList[actualPosition + 1]
+            value = instructionsList[actualPosition + 2]
+            if (alphabeticVariable(variable) == False or variable.lower() != variable) or integerValue(value) == False: #Revisa si no son correctos los parametros
+                validInstruction = False
+            else: #Si lo son, se agrega la variable al diccionario
+                definedVariables[variable] = value
+        if word == "TO":
+            if type(instructionsList[actualPosition + 1]) == str: #Si la funcion tiene un nombre en string asignado, comienza la verificación
+                followup = True
+                advanceNumber = 2
+                parametersNumber = 0
+                while followup == True: #Mientras hayan parametros a agregar, se ejecuta la instruccion
+                    posibleParameter = instructionsList[actualPosition + advanceNumber]
+                    if posibleParameter.count(':') == 1 and posibleParameter[0] == ':': #Si es un parametro, se añade al numero de parametros y se aavnza
+                        parametersNumber += 1
+                        advanceNumber += 1
+                    elif posibleParameter == "OUTPUT": #Si se llega a la instruccion OUTPUT se detiene la revision de parametros
+                        followup = False
+                    else: #Si hay una instruccion extra o una variable mal declarada se detiene el ciclo y se invalida la instruccion 
+                        followup = False
+                        validInstruction = False
+            else: #Si el nombre no es valido, se invalida la instruccion
+                validInstruction = False
         if char in specialChars.keys():
             specialChars[char] += 1
         if char == " " or char == "\n" or char in specialChars.keys():  #Si el caracter es un espacio o salto de línea, se ignora
@@ -83,6 +136,7 @@ def verifyInstructions():
                 word = ""
             else:
                 word = ""
+        actualPosition+= 1
     #Verificación de que haya la misma cantidad de caracteres de apertura como de cierre            
     if specialChars['('] == specialChars[')']:
         pass
